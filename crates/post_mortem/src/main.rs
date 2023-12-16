010b483f611322eb3b86c88177b5f4d0fce08a8f
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2023-12-15 16:40:32 -0800

    skip executable from args

--- crates/post_mortem/src/main.rs
+++ crates/post_mortem/src/main.rs
@@ -2,7 +2,7 @@ use post_mortem::*;
 use std::env;
 
 fn main() {
-    if let Err(err) = real_main(env::args_os()) {
+    if let Err(err) = real_main(env::args_os().skip(1)) {
         error::report(&err);
     }
 }
